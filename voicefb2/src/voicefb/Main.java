8f89e77c6b207451ae617ccb00a18913865ca891
Deepan Saravanan <deepan.saravanan83@gmail.com>
2016-04-25 02:35:50 -0400

    added Main.java file for calling fb library functions created in Posts.java

---
+++ voicefb2/src/voicefb/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main (String[] args) {
+        if (args.length == 1) {
+            String status = args[0]; 
+            Posts.updateStatus(status); 
+        } else if (args.length == 3) {
+            int function = Integer.parseInt(args[0]); 
+            if (function == 1) {
+                String status = args[1]; 
+                String photo = args[2]; 
+                Posts.postPhoto(photo, status); 
+            } else if (function == 2) {
+                String status = args[1];
+                String video = args[2]; 
+                Posts.postVideo(video, status); 
+            } else {
+                System.exit("invalid function signifier");
+            }
+        } else {
+            System.exit("invalid input"); 
+        }
+    }
+}
